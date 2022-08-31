275e305a13922b05fd05a04c48be49f11628e81a
Shazzadul Islam <shazzadulislam998@gmail.com>
2022-08-31 17:21:57 +0600

    created addToCart and removeFromCart functionality

--- resources/js/pages/invoices/Create.vue
+++ resources/js/pages/invoices/Create.vue
@@ -8,17 +8,54 @@ const router = useRouter();
 
 let form = ref([]);
 let customers = ref([]);
+let item = ref([]);
+let listCart = ref([]);
+let products = ref([]);
+
+const isModalOpen = ref(false);
 
 onMounted(async () => {
     getDefaultData();
     getCustomers();
+    getProducts();
 });
+
+const openModal = () => {
+    isModalOpen.value = true;
+};
+
+const closeModal = () => {
+    isModalOpen.value = false;
+};
+
+const addToCart = (item) => {
+    const itemcart = {
+        id: item.id,
+        item_code: item.item_code,
+        item_description: item.item_description,
+        unit_price: item.unit_price,
+        quantity: item.quantity,
+    };
+
+    listCart.value.push(itemcart);
+
+    closeModal();
+};
+
+const removeFromCart = (index) => {
+    listCart.value.splice(index, 1);
+};
+
+const getProducts = async () => {
+    let response = await axios.get("/api/products");
+    console.log(response.data);
+    products.value = response.data.products;
+};
 
 const getCustomers = async () => {
     let response = await axios.get("/api/customers");
 
     customers.value = response.data.customers;
-    console.log(customers.value);
 };
 
 const getDefaultData = async () => {
@@ -53,6 +90,7 @@ const viewInvoices = () => {
             <span class="text-gray-700 dark:text-gray-400"> Customer </span>
             <select
                 class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray rounded-md"
+                v-model="form.customer_id"
             >
                 <option>Select A Customer</option>
                 <option
@@ -67,10 +105,11 @@ const viewInvoices = () => {
         </label>
 
         <label class="block text-sm">
-            <span class="text-gray-700 dark:text-gray-400">Name</span>
+            <span class="text-gray-700 dark:text-gray-400">Number</span>
             <input
                 class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md"
                 placeholder="Jane Doe"
+                v-model="form.number"
             />
         </label>
 
@@ -79,6 +118,7 @@ const viewInvoices = () => {
             <input
                 class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md"
                 placeholder="Jane Doe"
+                v-model="form.reference"
             />
         </label>
 
@@ -88,6 +128,7 @@ const viewInvoices = () => {
                 class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md"
                 placeholder="Jane Doe"
                 type="date"
+                v-model="form.date"
             />
         </label>
         <label class="block text-sm">
@@ -96,6 +137,7 @@ const viewInvoices = () => {
                 class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md"
                 placeholder="Jane Doe"
                 type="date"
+                v-model="form.due_date"
             />
         </label>
 
@@ -127,53 +169,43 @@ const viewInvoices = () => {
                     </tr>
                 </thead>
                 <tbody class="divide-y dark:divide-gray-700">
-                    <tr class="text-gray-700 dark:text-gray-400">
+                    <tr
+                        class="text-gray-700 dark:text-gray-400"
+                        v-for="(itemcart, i) in listCart"
+                        :key="itemcart.id"
+                    >
                         <td class="px-4 py-3">
                             <div class="flex items-center text-sm">
-                                <!-- Avatar with inset shadow -->
-                                <div
-                                    class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
-                                >
-                                    <img
-                                        class="object-cover w-full h-full rounded-full"
-                                        src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                                        alt=""
-                                        loading="lazy"
-                                    />
-                                    <div
-                                        class="absolute inset-0 rounded-full shadow-inner"
-                                        aria-hidden="true"
-                                    ></div>
-                                </div>
-                                <div>
-                                    <p class="font-semibold">Maria Ramovic</p>
-                                    <p
-                                        class="text-xs text-gray-600 dark:text-gray-400"
-                                    >
-                                        Runner
-                                    </p>
-                                </div>
+                                #{{ itemcart.item_code }}
+                                {{ itemcart.item_description }}
                             </div>
                         </td>
                         <td class="px-4 py-3 text-sm w-44 text-left">
                             <input
                                 type="text"
                                 class="block w-4/6 mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md"
+                                v-model="itemcart.unit_price"
                             />
                         </td>
                         <td class="px-4 py-3 text-sm w-44 mr-4">
                             <input
                                 type="text"
                                 class="block w-4/6 mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input rounded-md"
+                                v-model="itemcart.quantity"
                             />
                         </td>
                         <td class="px-4 py-3 text-sm">
                             <div class="flex items-center justify-between">
-                                <span>$ 45233.43</span>
+                                <span v-if="itemcart.quantity">
+                                    ${{
+                                        itemcart.unit_price * itemcart.quantity
+                                    }}</span
+                                ><span v-else> $0000</span>
 
                                 <button
                                     class="flex items-center justify-between px-2 py-2 text-lg font-medium leading-5 text-red-500 rounded-lg focus:outline-none focus:shadow-outline-gray"
                                     aria-label="Delete"
+                                    @click="removeFromCart(i)"
                                 >
                                     <i class="bx bxs-trash"></i>
                                 </button>
@@ -186,7 +218,7 @@ const viewInvoices = () => {
     </div>
 
     <div class="flex justify-between mt-20 mb-4">
-        <ButtonLink>
+        <ButtonLink @click="openModal()">
             Add Product
             <i class="bx bx-plus"></i>
         </ButtonLink>
@@ -219,4 +251,96 @@ const viewInvoices = () => {
             </div>
         </div>
     </div>
+
+    <!-- Modal backdrop. This what you want to place close to the closing body tag -->
+    <div
+        v-if="isModalOpen"
+        class="fixed inset-0 z-30 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
+    >
+        <!-- Modal -->
+        <div
+            v-if="isModalOpen"
+            v-click-away="closeModal"
+            @keydown.escape="closeModal"
+            class="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl"
+            role="dialog"
+            id="modal"
+        >
+            <!-- Remove header if you don't want a close icon. Use modal body to place modal tile. -->
+            <header class="flex justify-end">
+                <button
+                    class="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700"
+                    aria-label="close"
+                    @click="closeModal"
+                >
+                    <svg
+                        class="w-4 h-4"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        role="img"
+                        aria-hidden="true"
+                    >
+                        <path
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                            fill-rule="evenodd"
+                        ></path>
+                    </svg>
+                </button>
+            </header>
+            <!-- Modal body -->
+            <div class="mt-4 mb-6">
+                <!-- Modal title -->
+                <p
+                    class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300"
+                >
+                    Add Product
+                </p>
+                <!-- Modal description -->
+                <div class="text-sm text-gray-700 dark:text-gray-400">
+                    <ul>
+                        <li
+                            class="grid grid-cols-[50px_minmax(100px,_1fr)_100px_50px] my-2"
+                        >
+                            <span>SL</span>
+                            <span>Product description</span>
+                            <span>Unit Price</span>
+                            <span></span>
+                        </li>
+                        <li
+                            class="grid grid-cols-[50px_minmax(100px,_1fr)_100px_50px] my-2"
+                            v-for="(product, i) in products"
+                            :key="product.id"
+                        >
+                            <span>{{ i + 1 }}</span>
+                            <span>{{ product.item_code }}</span>
+                            <span>{{ product.unit_price }} </span>
+                            <button
+                                class="dark:bg-gray-300 dark:text-gray-800 rounded-md"
+                                @click="addToCart(product)"
+                            >
+                                <i class="bx bx-plus"></i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <footer
+                class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800"
+            >
+                <button
+                    @click="closeModal"
+                    class="w-full px-5 py-3 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
+                >
+                    Cancel
+                </button>
+                <button
+                    class="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                >
+                    Accept
+                </button>
+            </footer>
+        </div>
+    </div>
+    <!-- End of modal backdrop -->
 </template>
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::resource('/invoices', InvoiceController::class);
 Route::get('/search-invoices', [InvoiceController::class, 'search_invoice']);
 Route::resource('/customers', CustomerController::class);
+Route::resource('/products', ProductController::class);
