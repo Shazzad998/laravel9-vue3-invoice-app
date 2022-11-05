5b005e82815e4e90212ec5a7b6358df43c8059bf
Shazzadul Islam <shazzadulislam998@gmail.com>
2022-11-05 19:41:12 +0600

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html  lang="en">
-  <head>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Invoice</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 
     {{-- Boxicon CDN --}}
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
@@ -15,14 +13,13 @@
     @vite('resources/css/app.css')
 
 
-  </head>
-  <body>
+</head>
+
+<body>
 
     <div id="app">
 
-        <div class=" text-4xl text-black dark:text-white">
-            asdfsadfsadfasdf
-        </div>
+
 
         <div>
 
@@ -40,6 +37,6 @@
     {{-- <script src="{{ asset('assets/js/focus-trap.js') }}" defer></script> --}}
 
     @vite('resources/js/app.js')
-  </body>
+</body>
 
 </html>
